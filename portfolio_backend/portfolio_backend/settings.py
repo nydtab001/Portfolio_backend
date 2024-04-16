fda5d61c008a41aa20f254c28de696d80f47563c
nydtab001 <53115370+nydtab001@users.noreply.github.com>
2024-04-16 13:08:30 +0200

    Update settings.py

--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfoliobackend-8lrwnqd8.b4a.run/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['portfoliobackend-8lrwnqd8.b4a.run', 'localhost', '127.0.0.1']
 
 
 # Application definition
